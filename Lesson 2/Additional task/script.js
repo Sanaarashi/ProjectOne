92b18a2827bd6a5d1733c7b01eb38fa6ad40e78c
Dmitry Zharikov <xenoffex@gmail.com>
2019-04-20 17:56:12 +0300

    fourth commit

--- Lesson 2/Additional task/script.js
+++ Lesson 2/Additional task/script.js
@@ -1,44 +1,34 @@
 'use strict';
 
-let date = new Date();
-let newDate = date.getDay();
-
-let daysOfTheWeek = ["Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
-];
-
-let arrayWithNumbers = ["322042",
-    "4210",
-    "8832",
-    "7993292",
-    "99232",
-    "143222234",
-    "3255532"
-];
+let date = new Date(),
+    newDate = date.getDay(),
+    daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    arrayWithNumbers = ["322042", "4210", "8832", "7993292", "99232", "143222234", "3255532"];
 
 arrayWithNumbers.forEach(element => {
-    let i = element.slice( 0,1);
-    if (i == 3 || i == 7) {
+    if (element.slice(0, 1) == 3 || element.slice(0, 1) == 7) {
         console.log(element);
     }
 });
 
 function showDays() {
-    let arr = document.getElementsByTagName('p');
-    for (let i = 0; i < arr.length; i++) {
-        arr[i].innerHTML = daysOfTheWeek[i];
-        if (i == 5 || i == 6) {
-            arr[i].innerHTML = `<b>${daysOfTheWeek[i]}</b>`;
+    let div = document.createElement("div"),
+        paragraph,
+        temp = 1;
+    div.id = 'main-div';
+    document.body.appendChild(div);
+    for (let i = 0; i < daysOfTheWeek.length; i++) {
+        paragraph = document.createElement('p');
+        paragraph.innerHTML = `${daysOfTheWeek[i]}`;
+        div.appendChild(paragraph);
+        if (paragraph.innerHTML == "Saturday" || paragraph.innerHTML == "Sunday") {
+            paragraph.style.cssText = "font-weight: bold";
         }
-        if (newDate == 6 || newDate == 7) {
-            arr[newDate - 1].innerHTML = `<b><i>${daysOfTheWeek[newDate-1]}</i></b>`;
-        } else {
-            arr[newDate - 1].innerHTML = `<i>${daysOfTheWeek[newDate-1]}</i>`;
-        };
+        if (temp == newDate && (newDate == 6 || newDate == 7)) {
+            paragraph.style.cssText = "font-weight: bold; font-style: italic";
+        } else if(temp == newDate) {
+            paragraph.style.cssText = "font-style: italic";
+        }
+        temp++;
     }
 };
